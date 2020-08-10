347632b0097ceea25fe64c7f5ac73f9815b041e3
TranRed <kupfer.nils@gmail.com>
2020-08-10 16:28:23 +0200

    first experimential deck tracker plugin version
    Updates MMR and placement when entering the menu after the game

--- HDTPlugin/BGOverlayUpdater/BGOverlayUpdater/overlayUpdater.cs
+++ HDTPlugin/BGOverlayUpdater/BGOverlayUpdater/overlayUpdater.cs
@@ -4,16 +4,95 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.IO;
+using Hearthstone_Deck_Tracker.Plugins;
+using Hearthstone_Deck_Tracker.Enums;
 using Hearthstone_Deck_Tracker.Hearthstone;
 using Hearthstone_Deck_Tracker.API;
-using Hearthstone_Deck_Tracker.Plugins;
+using Hearthstone_Deck_Tracker.Hearthstone.Entities;
+using Hearthstone_Deck_Tracker.Utility;
+using Hearthstone_Deck_Tracker.Utility.Logging;
+using HearthDb.Enums;
+using System.Runtime.ExceptionServices;
 
 namespace BGOverlayUpdater
 {
     public class overlayUpdater
     {
-        internal static void GameEnd()
+        static int first = 0;
+        static int second = 0;
+        static int third = 0;
+        static int fourth = 0;
+        static int fifth = 0;
+        static int sixth = 0;
+        static int seventh = 0;
+        static int eigth = 0;
+        static string mmrStart = "4,000";
+
+        internal static void InMenu()
         {
+
+            //update rating in Menu after battlegrounds game
+            
+            int rating = Core.Game.CurrentGameStats.BattlegroundsRatingAfter;
+            string ratingStr = rating.ToString();
+            ratingStr = ratingStr.Substring(0, ratingStr.Length - 3) + "," + ratingStr.Substring(ratingStr.Length - 3, 3);
+
+            int playerId = Core.Game.Player.Id;
+            Entity hero = Core.Game.Entities.Values.Where(x => x.IsHero && x.GetTag(GameTag.PLAYER_ID) == playerId).First();
+
+            var placement = hero.GetTag(GameTag.PLAYER_LEADERBOARD_PLACE);
+            switch (placement)
+            {
+                case 1:
+                    first = first + 1;
+                    break;
+
+                case 2:
+                    second = second + 1;
+                    break;
+
+                case 3:
+                    third = third + 1;
+                    break;
+
+                case 4:
+                    fourth = fourth + 1;
+                    break;
+
+                case 5:
+                    fifth = fifth + 1;
+                    break;
+
+                case 6:
+                    sixth = sixth + 1;
+                    break;
+
+                case 7:
+                    seventh = seventh + 1;
+                    break;
+
+                case 8:
+                    eigth = eigth + 1;
+                    break;
+            }
+            string placementStr = placement.ToString();
+
+            //experimental code to update the js
+            //path should be set in settings (somehow) and placements and MMR need to be read from result
+            //starting MMR to be set on startup
+            var path = @"C:\Users\kupfe\OneDrive\Dokumente\GitHub\bgbrowsersource\scripts\main.js";
+            string[] lines = { "document.getElementById(\"MMRstart\").innerHTML = \""+ mmrStart +"\";",
+                               "document.getElementById(\"MMRnow\").innerHTML = \""+ ratingStr +"\";",
+                               "document.getElementById(\"first\").innerHTML = \""+ first.ToString() +"\";",
+                               "document.getElementById(\"second\").innerHTML = \""+ second.ToString() +"\";",
+                               "document.getElementById(\"third\").innerHTML = \""+ third.ToString() +"\";",
+                               "document.getElementById(\"fourth\").innerHTML = \""+ fourth.ToString() +"\";",
+                               "document.getElementById(\"fifth\").innerHTML = \""+ fifth.ToString() +"\";",
+                               "document.getElementById(\"sixth\").innerHTML = \""+ sixth.ToString() +"\";",
+                               "document.getElementById(\"seventh\").innerHTML = \""+ seventh.ToString() +"\";",
+                               "document.getElementById(\"eigth\").innerHTML = \""+ eigth.ToString() +"\";",};
+            File.WriteAllLines(path, lines);
         }
 
     }
@@ -23,7 +102,8 @@ namespace BGOverlayUpdater
         public void OnLoad()
         {
             // Triggered upon startup and when the user ticks the plugin on
-            GameEvents.OnGameEnd.Add(overlayUpdater.GameEnd);
+            //GameEvents.OnGameEnd.Add(overlayUpdater.GameEnd);
+            GameEvents.OnInMenu.Add(overlayUpdater.InMenu);
         }
 
         public void OnUnload()
