04164e41bc3324a50c760e0108227acf7a87ddcd
TranRed <kupfer.nils@gmail.com>
2020-08-11 19:01:44 +0200

    Added file dialog
    added button for file dialog
    made textfield for file location read-only
    fixes #2

--- HDTPlugin/BGOverlayUpdater/BGOverlayUpdater/SettingsControl.xaml.cs
+++ HDTPlugin/BGOverlayUpdater/BGOverlayUpdater/SettingsControl.xaml.cs
@@ -1,5 +1,8 @@
 using System.Windows;
 using System.Windows.Controls;
+using Microsoft.Win32;
+using System;
+using System.IO;
 
 namespace BGOverlayUpdater
 {
@@ -20,6 +23,22 @@ namespace BGOverlayUpdater
         void resetClicked(object sender, RoutedEventArgs e)
         {
             overlayUpdater.resetSession();
+        }
+
+        void fileDialogClicked(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            
+
+            int lastSlash = jsFileLocation.Text.LastIndexOf("\\");
+            fileDialog.InitialDirectory = jsFileLocation.Text.Substring(0, lastSlash);
+            fileDialog.Filter = "JavaScript Files (.js)|*.js";
+
+            if (fileDialog.ShowDialog() == true)
+            {
+                jsFileLocation.Text = fileDialog.FileName;
+            }
+            
         }
 
     }
